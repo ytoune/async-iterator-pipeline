00ecd9ea74b401cb414b48b53aac7bdd5f419949
publisher <tone@re-knock.io>
2020-05-21 01:48:51 +0900

    Release v2.1.0!!

--- IterUtil.js
+++ IterUtil.js
@@ -1,5 +1,40 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var IterUtilImpl_1 = require("./IterUtilImpl");
-Object.defineProperty(exports, "IterUtil", { enumerable: true, get: function () { return IterUtilImpl_1.IterUtilImpl; } });
-Object.defineProperty(exports, "default", { enumerable: true, get: function () { return IterUtilImpl_1.IterUtilImpl; } });
+exports.IterUtil = exports.iterutil = void 0;
+exports.iterutil = (iter) => new IterUtil(iter);
+class IterUtil {
+    constructor(_list) {
+        this._list = _list;
+    }
+    [Symbol.asyncIterator]() {
+        return this._list[Symbol.asyncIterator]();
+    }
+    async first() {
+        for await (const i of this._list)
+            return i;
+    }
+    async last() {
+        let l;
+        for await (const i of this._list)
+            l = i;
+        return l;
+    }
+    async toArray() {
+        const list = [];
+        for await (const i of this._list)
+            list.push(i);
+        return list;
+    }
+    async forEach(f) {
+        for await (const i of this._list)
+            await f(i);
+    }
+    pipe(...ops) {
+        // @ts-ignore
+        let tmp = this._list;
+        for (const op of ops)
+            tmp = op(tmp);
+        return new IterUtil(tmp);
+    }
+}
+exports.IterUtil = IterUtil;
--- libs/WrapIter.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class WrapIter<T> {
-    private readonly iter;
-    cur: number;
-    done: boolean;
-    constructor(iter: AsyncIterator<T>);
-    next(): Promise<IteratorResult<T, any>>;
-}
